35ee521ee26bc5b9dd25cc3f7e040edf9aaaee03
Matti Tolonen <mattitolonen@Matti-Mac-mini.local>
2025-11-11 11:21:57 +0200

    workflow adjustment on environment

--- tf_rb_bootstrap/output.tf
+++ tf_rb_bootstrap/output.tf
@@ -9,3 +9,17 @@ output "ARM_CLIENT_ID" {
 output "ARM_TENANT_ID" {
   value = data.azurerm_client_config.current.tenant_id
 }
+
+
+output "TF_VAR_RG_NAME" {
+    value = azurerm_resource_group.tfstate_remotebackend.name
+}
+
+output "TF_VAR_STORAGE_NAME" {
+    value = azurerm_storage_account.tfstate.name
+}
+
+output "TF_VAR_CONTAINER_NAME" {
+  value = azurerm_storage_container.tfstate.name
+}
+
